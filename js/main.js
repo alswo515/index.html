7ebed3a3ea85e3111da0c7f2111d475e213c9752
alswo515 <67941961+alswo515@users.noreply.github.com>
2020-09-11 16:58:11 +0900

    버튼이벤트 시작

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 (function ($) {
-
+    $('.older_posts > form > button').on('click', function (e) {
+        e.preventDefault()
+        $('article4').removeClass('blind')
+        return false
+    })
 })(jquery)
