d9fbfdf033e73c3dc5a751a12d953b310df652a7
alswo515 <67941961+alswo515@users.noreply.github.com>
2020-09-11 16:12:24 +0900

    메뉴바슬라이드

--- js/index.js
+++ js/index.js
@@ -4,7 +4,16 @@
             $(this).find(".depth2").stop().slideDown(700);
         },
         function () {
-            $(this).find(".depth2").stop().slideUp(500);
+            $(this).find(".depth2").stop().slideUp(500).css({
+                textAlign: 'center',
+                display: 'inline-block',
+                position: 'absolute',
+                top: '100%',
+                right: "0",
+                display: 'none',
+                lineHeight: '2',
+                zIndex: '999'
+            });
         }
     );
 })(jQuery)
