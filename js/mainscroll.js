dbf92b85d907fde2314c252e3a4fd4cc705e3347
alswo515 <67941961+alswo515@users.noreply.github.com>
2020-09-15 11:45:17 +0900

    메인페이지윈도우스크롤문제

--- js/mainscroll.js
+++ js/mainscroll.js
@@ -1,9 +1,7 @@
 $(window).scroll(function () {
     var sct = $(this).scrollTop()
-    //actNear 는 스크롤 탑값이 너무 크므로 .act값을 잡아서 스크롤값이 절반만와도 이벤트가 발생하도록 해주는 변수 
     var actNear = $('.article2').offset().top - $(this).height() / 2
     console.log(actNear)
-    //actul 관련 스크롤 이벤트
     if (sct >= actNear) {
         $(".article2").addClass("on");
     } else {
