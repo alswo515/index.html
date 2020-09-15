9253a676a766bfac7b719f64fd1e439fe0d6ef62
alswo515 <67941961+alswo515@users.noreply.github.com>
2020-09-15 17:09:07 +0900

    버튼이벤트추가

--- js/index.js
+++ js/index.js
@@ -31,4 +31,26 @@
             scrollTop: 0
         }, 100, 'linear')
     }) //gotop버튼클릭시 스크린 탑값을 맨위로
+
+    // $('.older_posts a').on('click', function (e) {
+    //     e.preventDefault()
+    //     $('.article5').removeClass('blind')
+    //     $('.article6').removeClass('blind')
+    //     $('.article7').removeClass('blind')
+    // })
+
+    $(".older_posts a").toggle(
+        function () {
+            $('.article5').removeClass('blind')
+            $('.article6').removeClass('blind')
+            $('.article7').removeClass('blind')
+        },
+        function () {
+            $('.article5').addClass('blind')
+            $('.article6').addClass('blind')
+            $('.article7').addClass('blind')
+        }
+    )
+
+
 })(jQuery)
--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
 (function ($) {
-    $('.older_posts > a').on('click', function (e) {
-        e.preventDefault()
-        $('article5').addClass('blind')
-        $('article6').addClass('blind')
-        $('article7').addClass('blind')
-        return false
-    })
+
 })(jQuery)
