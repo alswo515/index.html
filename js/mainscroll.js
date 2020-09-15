174dc39432525b399c93da49f3859d5cda011ad0
alswo515 <67941961+alswo515@users.noreply.github.com>
2020-09-15 15:27:21 +0900

    로딩화면 및 배경들 애니메이션

--- js/mainscroll.js
+++ js/mainscroll.js
@@ -3,6 +3,9 @@ $(window).scroll(function () {
     var actNear = $('.article2').offset().top - $(this).height() / 2
     var act3Near = $('.article3').offset().top - $(this).height() / 2
     var act4Near = $('.article4').offset().top - $(this).height() / 2
+    var act5Near = $('.article5').offset().top - $(this).height() / 2
+    var act6Near = $('.article6').offset().top - $(this).height() / 2
+    var act7Near = $('.article7').offset().top - $(this).height() / 2
     console.log(actNear)
     if (sct >= actNear) {
         $(".article2").addClass("on");
@@ -18,6 +21,21 @@ $(window).scroll(function () {
         $(".article4").addClass("on");
     } else {
         $(".article4").removeClass("on");
+    }
+    if (sct >= act5Near) {
+        $(".article5").addClass("on");
+    } else {
+        $(".article5").removeClass("on");
+    }
+    if (sct >= act6Near) {
+        $(".article6").addClass("on");
+    } else {
+        $(".article6").removeClass("on");
+    }
+    if (sct >= act7Near) {
+        $(".article7").addClass("on");
+    } else {
+        $(".article7").removeClass("on");
     }
 
 })
