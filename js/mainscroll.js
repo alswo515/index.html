83a7fa3706650620691123948bce06c7e82b56a9
alswo515 <67941961+alswo515@users.noreply.github.com>
2020-09-21 12:18:24 +0900

    스크롤액션 사라지는거태그

--- js/mainscroll.js
+++ js/mainscroll.js
@@ -10,37 +10,37 @@ $(window).scroll(function () {
     console.log(actNear)
     if (sct >= actNear) {
         $(".article2").addClass("on");
-    } else {
+    } else if (sct === 0) {
         $(".article2").removeClass("on");
     }
     if (sct >= act3TitNear) {
         $(".article3_tit").addClass("on");
-    } else {
+    } else if (sct === 0) {
         $(".article3_tit").removeClass("on");
     }
     if (sct >= act3Near) {
         $(".article3").addClass("on");
-    } else {
+    } else if (sct === 0) {
         $(".article3").removeClass("on");
     }
     if (sct >= act4Near) {
         $(".article4").addClass("on");
-    } else {
+    } else if (sct === 0) {
         $(".article4").removeClass("on");
     }
     if (sct >= act5Near) {
         $(".article5").addClass("on");
-    } else {
+    } else if (sct === 0) {
         $(".article5").removeClass("on");
     }
     if (sct >= act6Near) {
         $(".article6").addClass("on");
-    } else {
+    } else if (sct === 0) {
         $(".article6").removeClass("on");
     }
     if (sct >= act7Near) {
         $(".article7").addClass("on");
-    } else {
+    } else if (sct === 0) {
         $(".article7").removeClass("on");
     }
 
