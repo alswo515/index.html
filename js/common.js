128357053f6f67163d23451748ead6f5023ace9b
alswo515 <67941961+alswo515@users.noreply.github.com>
2020-09-21 10:32:29 +0900

    js파일정리

--- js/common.js
+++ js/common.js
@@ -25,6 +25,35 @@
 
   }
 
+  $(".depth1 > li").hover(
+    function () {
+      $(this).find(".depth2").stop().slideDown(500);
+    },
+    function () {
+      $(this).find(".depth2").stop().slideUp(500)
+    }
+  );
+  var sct = 0;
+  $(window).scroll(function () {
+    sct = $(this).scrollTop();
+
+    //scrollTop() 값이 100이사잉 되면 맨위로 버튼보이고 ,100미만이면 숨기기
+    if (sct >= 100) {
+      $('.gotop').addClass('on').stop().animate({
+        opacity: '1'
+      }, 500)
+    } else {
+      $('.gotop').removeClass('on').stop().animate({
+        opacity: '0'
+      }, 500)
+    }
+  });
+
+  $('.gotop').on('click', function () {
+    $('body, html').stop().animate({
+      scrollTop: 0
+    }, 100, 'linear')
+  })
   // var scollSize = $(document).height() - $(window).height();
   // var delayBar;
   // $(window).on("scroll", function () {
