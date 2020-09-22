6c7482f4267d879758330668f94a5508e22c8633
alswo515 <67941961+alswo515@users.noreply.github.com>
2020-09-22 12:30:49 +0900

    서브페이지 스크롤 관련 문제 해결

--- js/common.js
+++ js/common.js
@@ -172,74 +172,71 @@
 
 
   function projectScroll() {
-    $(window).scroll(function () {
-      var project = $('.project_1').offset().top - $(this).height() / 2
-      var project2 = $('.project_2').offset().top - $(this).height() / 2
-      var projectTit = $('.project_tit').offset().top - $(this).height() / 2
-      if (sct >= projectTit) {
-        $(".project_tit").addClass("on");
-      } else if (sct === 0) {
-        $(".project_tit").removeClass("on");
-      }
-      if (sct >= project) {
-        $(".project_1").addClass("on");
-      } else if (sct === 0) {
-        $(".project_1").removeClass("on");
-      }
-      if (sct >= project2) {
-        $(".project_2").addClass("on");
-      } else if (sct === 0) {
-        $(".project_2").removeClass("on");
-      }
-    })
+    sct = $(this).scrollTop()
+    var project = $('.project_1').offset().top - $(this).height() / 2
+    var project2 = $('.project_2').offset().top - $(this).height() / 2
+    var projectTit = $('.project_tit').offset().top - $(this).height() / 2
+    if (sct >= projectTit) {
+      $(".project_tit").addClass("on");
+    } else if (sct === 0) {
+      $(".project_tit").removeClass("on");
+    }
+    if (sct >= project) {
+      $(".project_1").addClass("on");
+    } else if (sct === 0) {
+      $(".project_1").removeClass("on");
+    }
+    if (sct >= project2) {
+      $(".project_2").addClass("on");
+    } else if (sct === 0) {
+      $(".project_2").removeClass("on");
+    }
   }
 
   function sectionScroll() {
-    $(window).scroll(function () {
-      var sct = $(this).scrollTop()
-      var actNear = $('.article2').offset().top - $(this).height() / 2
-      var act3Near = $('.article3').offset().top - $(this).height() / 2
-      var act3TitNear = $('.article3_tit').offset().top - $(this).height() / 2
-      var act4Near = $('.article4').offset().top - $(this).height() / 2
-      var act5Near = $('.article5').offset().top - $(this).height() / 2
-      var act6Near = $('.article6').offset().top - $(this).height() / 2
-      var act7Near = $('.article7').offset().top - $(this).height() / 2
-      if (sct >= actNear) {
-        $(".article2").addClass("on");
-      } else if (sct === 0) {
-        $(".article2").removeClass("on");
-      }
-      if (sct >= act3TitNear) {
-        $(".article3_tit").addClass("on");
-      } else if (sct === 0) {
-        $(".article3_tit").removeClass("on");
-      }
-      if (sct >= act3Near) {
-        $(".article3").addClass("on");
-      } else if (sct === 0) {
-        $(".article3").removeClass("on");
-      }
-      if (sct >= act4Near) {
-        $(".article4").addClass("on");
-      } else if (sct === 0) {
-        $(".article4").removeClass("on");
-      }
-      if (sct >= act5Near) {
-        $(".article5").addClass("on");
-      } else if (sct === 0) {
-        $(".article5").removeClass("on");
-      }
-      if (sct >= act6Near) {
-        $(".article6").addClass("on");
-      } else if (sct === 0) {
-        $(".article6").removeClass("on");
-      }
-      if (sct >= act7Near) {
-        $(".article7").addClass("on");
-      } else if (sct === 0) {
-        $(".article7").removeClass("on");
-      }
-    })
+    sct = $(this).scrollTop()
+    var actNear = $('.article2').offset().top - $(this).height() / 2
+    var act3Near = $('.article3').offset().top - $(this).height() / 2
+    var act3TitNear = $('.article3_tit').offset().top - $(this).height() / 2
+    var act4Near = $('.article4').offset().top - $(this).height() / 2
+    var act5Near = $('.article5').offset().top - $(this).height() / 2
+    var act6Near = $('.article6').offset().top - $(this).height() / 2
+    var act7Near = $('.article7').offset().top - $(this).height() / 2
+    if (sct >= actNear) {
+      $(".article2").addClass("on");
+    } else if (sct === 0) {
+      $(".article2").removeClass("on");
+    }
+    if (sct >= act3TitNear) {
+      $(".article3_tit").addClass("on");
+    } else if (sct === 0) {
+      $(".article3_tit").removeClass("on");
+    }
+    if (sct >= act3Near) {
+      $(".article3").addClass("on");
+    } else if (sct === 0) {
+      $(".article3").removeClass("on");
+    }
+    if (sct >= act4Near) {
+      $(".article4").addClass("on");
+    } else if (sct === 0) {
+      $(".article4").removeClass("on");
+    }
+    if (sct >= act5Near) {
+      $(".article5").addClass("on");
+    } else if (sct === 0) {
+      $(".article5").removeClass("on");
+    }
+    if (sct >= act6Near) {
+      $(".article6").addClass("on");
+    } else if (sct === 0) {
+      $(".article6").removeClass("on");
+    }
+    if (sct >= act7Near) {
+      $(".article7").addClass("on");
+    } else if (sct === 0) {
+      $(".article7").removeClass("on");
+    }
   }
 
 
