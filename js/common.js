65bb3070072145035acbbd83d629b72b8ed521e8
alswo515 <67941961+alswo515@users.noreply.github.com>
2020-09-22 11:00:25 +0900

    프로젝트구역 시작

--- js/common.js
+++ js/common.js
@@ -19,9 +19,9 @@
       return false
     }
     //배열변수를 사용해서 글자색깔을 바꿔주는코드
-    $('.introAni span').text(k + '초').css({
-      color: colorCode[k]
-    });
+    // $('.introAni span').text(k + '초').css({
+    //   color: colorCode[k]
+    // });
 
   }
 
@@ -36,7 +36,6 @@
   var sct = 0;
   $(window).scroll(function () {
     sct = $(this).scrollTop();
-
     //scrollTop() 값이 100이사잉 되면 맨위로 버튼보이고 ,100미만이면 숨기기
     if (sct >= 100) {
       $('.gotop').addClass('on').stop().animate({
@@ -136,6 +135,37 @@
   });
 
 
+
+  //서브페이지에서 스크롤이벤트를 실행시킬려면
+  //서브페이지에서 스크롤이벤트 스크립트를 작성한후에
+  // common.js 에서 if문을 돌려서 조건을 추가해서 돌려야한다
+
+  // 예를 들면
+  // $(window).scroll(function(){
+  //    if ($("#newContainer").children().is("#main_section")) {
+  //          mainScroll();
+  //     }
+  //     if ($("#newContainer").children().is("#company_section")) {
+  //       companyScroll();
+  //     }
+  // })
+
+  //메인페이지의 스크롤 이벤트
+  // function mainScroll() {  메인페이지 해당구역들의 스크롤애니메이션 프로그램  }
+  //서브페이지의 스크롤이벤트 
+  // function companyScroll() {   컴퍼니페이지 해당구역들의 스크롤애니메이션 프로그램  }
+
+  // 처음에 한번 sct = $(window).scrollTop() 값 당연히 구해야 하구요.
+
+  $(window).scroll(function () {
+    if ($("#kimContainer").children().is("#project_section")) {
+      mainScroll();
+    }
+  })
+
+  function companyScroll() {
+    sct = $(window).scrollTop()
+  }
 
 
 })(jQuery);
