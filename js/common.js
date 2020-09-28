d586bd9a4eff3c24fb6e0b519625950843da87bd
alswo515 <67941961+alswo515@users.noreply.github.com>
2020-09-28 17:11:40 +0900

    반응형 시작

--- js/common.js
+++ js/common.js
@@ -66,8 +66,8 @@
   // });
   var flag = true;
   $(window).on("scroll", function () {
-    var scollSize = $(document).height() - $('#header').height() - $(window).height();
     //스크롤 탑값에따라 헤더아래쪽 선의 넓이 변경하기
+    var scollSize = $(document).height() - $('#header').height() - $(window).height();
     sct = $(this).scrollTop();
     var wid = (sct / scollSize) * 100 + '%';
     $(".scrolling-bar").css({
