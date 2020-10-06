c814760e1cd01c9d0a39e7f7fa014b19f4ffb551
alswo515 <67941961+alswo515@users.noreply.github.com>
2020-10-06 17:08:09 +0900

    js depth2 hover 이벤트

--- js/common.js
+++ js/common.js
@@ -25,7 +25,7 @@
     // });
   }
   //depth1 클릭시 depth2 보이게
-  $(".depth1 > li:nth-child(5) a").hover(
+  $(".depth1 > li").hover(
     function () {
       $(this).find(".depth2").stop().slideDown(500);
     },
@@ -88,8 +88,7 @@
   });
 
   $(".gotop").on("click", function () {
-    $("body, html").stop().animate(
-      {
+    $("body, html").stop().animate({
         scrollTop: 0,
       },
       100,
@@ -140,8 +139,7 @@
           zIndex: "9999999",
         })
         .stop()
-        .animate(
-          {
+        .animate({
             height: "100px",
             opacity: "1",
           },
@@ -157,8 +155,7 @@
           paddingBottom: "100px",
         })
         .stop()
-        .animate(
-          {
+        .animate({
             opacity: "1",
             height: "100px",
             backgroundColor: "rgba(255,255,255,0.9)",
@@ -170,15 +167,13 @@
 
     //scrollTop() 값이 100이사잉 되면 맨위로 버튼보이고 ,100미만이면 숨기기 g
     if (sct >= 100) {
-      $(".gotop").addClass("on").stop().animate(
-        {
+      $(".gotop").addClass("on").stop().animate({
           opacity: "1",
         },
         500
       );
     } else {
-      $(".gotop").removeClass("on").stop().animate(
-        {
+      $(".gotop").removeClass("on").stop().animate({
           opacity: "0",
         },
         500
